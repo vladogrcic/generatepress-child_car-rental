d4824c5f01c9f4fdf0857c83b740a3fd3bd41083
Vlado Grčić <vladogrcic1993@gmail.com>
2019-02-03 01:42:57 +0100

    Formatting code and changing file structure.

--- functions.php
+++ functions.php
@@ -13,10 +13,11 @@ include get_stylesheet_directory() . '/includes/cart.php';
  */
 function import_scripts()
 {
+    wp_enqueue_style('general-site', get_stylesheet_directory_uri() . '/css/general-site.css', 40, 1);
     wp_enqueue_script(
         'main',
         get_stylesheet_directory_uri() . '/js/front-end/main.js',
         array('jquery')
     );
 }
-add_action('wp_enqueue_scripts', 'import_scripts');
+add_action('wp_enqueue_scripts', 'import_scripts', 20, 1);
